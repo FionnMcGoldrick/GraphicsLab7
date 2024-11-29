cb11072aef2d9344c840f82ba7ec24bafd0d94be
Fionn McGoldrick <G00422349@atu.ie>
2024-11-29 09:40:43 +0000

    Original Image added, GrayScale Added, Corner Harris Added

--- Lab9/lab9.py
+++ Lab9/lab9.py
@@ -2,7 +2,23 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
+#Variables
+blockSize = 5
+aperture_size = 3
+k = 1
+
+
 #Original Image
 image = cv2.imread('ATU1.jpg')
 cv2.imshow('Origial', image)
+cv2.waitKey(0)
+
+#GrayScale Image
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Grayscale', gray_image)
+cv2.waitKey(0)
+
+#CornerHarris
+dst = cv2.cornerHarris(gray_image, blockSize, aperture_size, k)
+cv2.imshow('cornerHarris', dst)
 cv2.waitKey(0)
